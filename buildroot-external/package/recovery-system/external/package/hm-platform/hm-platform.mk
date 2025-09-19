64caf1050bd71d1a06302034db1cce9005a15b45
Jens Maus <mail@jens-maus.de>
2025-09-20 00:43:56 +0200

    fix correct platform being used in HM_PLATFORM_ARCH (#3211)
    This change modifies the HM_PLATFORM_ARCH variable in hm-platform.mk so
    that the correct 32bit/64bit binaries should be picked up from the OCCU
    repository. In addition, HM_PLATFORM_SITE uses the correct github
    "OpenCCU" organization now where our occu branch is located now.

--- buildroot-external/package/recovery-system/external/package/hm-platform/hm-platform.mk
+++ buildroot-external/package/recovery-system/external/package/hm-platform/hm-platform.mk
@@ -6,34 +6,40 @@
 #############################################################
 
 HM_PLATFORM_VERSION = $(OCCU_VERSION)
-HM_PLATFORM_SITE = $(call github,jens-maus,occu,$(OCCU_VERSION))
+HM_PLATFORM_SITE = $(call github,OpenCCU,occu,$(OCCU_VERSION))
+
+ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),)
+	$(error hm-platform requires a glibc toolchain (BR2_TOOLCHAIN_USES_GLIBC))
+endif
 
 ifeq ($(BR2_arm),y)
 	HM_PLATFORM_ARCH=arm-gnueabihf-gcc8
 endif
 
 ifeq ($(BR2_aarch64),y)
-	HM_PLATFORM_ARCH=arm-gnueabihf-gcc8
+	HM_PLATFORM_ARCH=aarch64-linux-gnu
 endif
 
 ifeq ($(BR2_i386),y)
-	HM_PLATFORM_ARCH=X86_32_GCC8
+	HM_PLATFORM_ARCH=i686-linux-gnu
 endif
 
 ifeq ($(BR2_x86_64),y)
-	HM_PLATFORM_ARCH=X86_32_GCC8
+	HM_PLATFORM_ARCH=x86_64-linux-gnu
 endif
 
+HM_PLATFORM_BETA_SUFFIX ?= -Beta
+
 define HM_PLATFORM_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis/bin/ssdpd $(TARGET_DIR)/bin/ssdpd
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis/bin/eq3configcmd $(TARGET_DIR)/bin/eq3configcmd
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis/bin/eq3configd $(TARGET_DIR)/bin/eq3configd
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis/lib/libeq3config.so $(TARGET_DIR)/lib/libeq3config.so
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD/bin/crypttool $(TARGET_DIR)/bin/crypttool
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD/lib/libLanDeviceUtils.so $(TARGET_DIR)/lib/libLanDeviceUtils.so
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD/lib/libUnifiedLanComm.so $(TARGET_DIR)/lib/libUnifiedLanComm.so
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD/lib/libelvutils.so $(TARGET_DIR)/lib/libelvutils.so
-	cp -a $(HM_PLATFORM_PKGDIR)/rootfs-overlay/* $(TARGET_DIR)/
+	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis$(HM_PLATFORM_BETA_SUFFIX)/bin/ssdpd $(TARGET_DIR)/bin/ssdpd
+	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis$(HM_PLATFORM_BETA_SUFFIX)/bin/eq3configcmd $(TARGET_DIR)/bin/eq3configcmd
+	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis$(HM_PLATFORM_BETA_SUFFIX)/bin/eq3configd $(TARGET_DIR)/bin/eq3configd
+	$(INSTALL) -D -m 0644 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis$(HM_PLATFORM_BETA_SUFFIX)/lib/libeq3config.so $(TARGET_DIR)/lib/libeq3config.so
+	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD$(HM_PLATFORM_BETA_SUFFIX)/bin/crypttool $(TARGET_DIR)/bin/crypttool
+	$(INSTALL) -D -m 0644 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD$(HM_PLATFORM_BETA_SUFFIX)/lib/libLanDeviceUtils.so $(TARGET_DIR)/lib/libLanDeviceUtils.so
+	$(INSTALL) -D -m 0644 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD$(HM_PLATFORM_BETA_SUFFIX)/lib/libUnifiedLanComm.so $(TARGET_DIR)/lib/libUnifiedLanComm.so
+	$(INSTALL) -D -m 0644 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD$(HM_PLATFORM_BETA_SUFFIX)/lib/libelvutils.so $(TARGET_DIR)/lib/libelvutils.so
+	cp -a $(HM_PLATFORM_PKGDIR)/rootfs-overlay/. $(TARGET_DIR)/
 endef
 
 define HM_PLATFORM_INSTALL_INIT_SYSV
