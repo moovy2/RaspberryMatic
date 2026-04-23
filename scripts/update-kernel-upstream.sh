2d811b985fc56e6a80deab26f38cb92a5da92d89
Jens Maus <mail@jens-maus.de>
2026-04-23 14:01:49 +0200

    fix legal-info runs for OCI/LXC platform (#3810)
    Fixes inconsistent hash file content. In addition, the update-kernel-upstream.sh
    script is now only replacing hash entries from the old to the new
    version.

--- scripts/update-kernel-upstream.sh
+++ scripts/update-kernel-upstream.sh
@@ -50,7 +50,7 @@ fi
 sed -i "s/BR2_LINUX_KERNEL_CUSTOM_VERSION_VALUE=\".*\"/BR2_LINUX_KERNEL_CUSTOM_VERSION_VALUE=\"${ID}\"/g" buildroot-external/configs/{oci_*,odroid-*,ova,generic-*,tinkerboard2}.config
 
 # update hash files
-sed -i "/${PACKAGE_NAME}-.*\.tar\.xz/d" "buildroot-external/patches/${PACKAGE_NAME}/${PACKAGE_NAME}.hash"
+sed -i "/${PACKAGE_NAME}-${CURRENT_VERSION}\.tar\.xz/d" "buildroot-external/patches/${PACKAGE_NAME}/${PACKAGE_NAME}.hash"
 echo "sha256  ${ARCHIVE_HASH}  ${PACKAGE_NAME}-${ID}.tar.xz" >>"buildroot-external/patches/${PACKAGE_NAME}/${PACKAGE_NAME}.hash"
-sed -i "/${PACKAGE_NAME}-.*\.tar\.xz/d" "buildroot-external/patches/${PACKAGE_NAME}-headers/${PACKAGE_NAME}-headers.hash"
+sed -i "/${PACKAGE_NAME}-${CURRENT_VERSION}\.tar\.xz/d" "buildroot-external/patches/${PACKAGE_NAME}-headers/${PACKAGE_NAME}-headers.hash"
 echo "sha256  ${ARCHIVE_HASH}  ${PACKAGE_NAME}-${ID}.tar.xz" >>"buildroot-external/patches/${PACKAGE_NAME}-headers/${PACKAGE_NAME}-headers.hash"
