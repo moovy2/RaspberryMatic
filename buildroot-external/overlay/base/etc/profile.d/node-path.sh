b1847569ba9ad83fe09716cf7e705f3c97b41b71
Jens Maus <mail@jens-maus.de>
2026-09-18 08:33:58 +0200

    fix /etc/profile.d variables to omit empty vars (#4199)
    This fixes the LD_LIBRARY_PATH, PATH and NODE_PATH definitions in
    /etc/profile.d to ensure that no empty colon is added when the variable
    is empty which could cause issues interpreting it as the current
    directoy/path and using unexpected pathes in the searches.

--- buildroot-external/overlay/base/etc/profile.d/node-path.sh
+++ buildroot-external/overlay/base/etc/profile.d/node-path.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # shellcheck shell=dash
-export NODE_PATH=/usr/local/lib/node_modules:/usr/lib/node_modules:${NODE_PATH}
+export NODE_PATH="/usr/local/lib/node_modules:/usr/lib/node_modules${NODE_PATH:+:${NODE_PATH}}"
