135c4205528cbbf8df4028110eb56fe6a0d60530
Jens Maus <mail@jens-maus.de>
2026-09-22 18:50:58 +0200

    build(openccu-base): migrate patch 0166 (#4229)
    Completes the cleanup for OpenCCU-Base PR OpenCCU/OpenCCU-Base#50, which migrated rootfs patch 0166-WebUI-Fix-PowermeterControl.patch into the native source.

--- buildroot-external/package/openccu-base/openccu-base.mk
+++ buildroot-external/package/openccu-base/openccu-base.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCCU_BASE_VERSION = 59e46830783d5517be449301ebf682d29b86e58e
+OPENCCU_BASE_VERSION = 41a8796a2869699f008f229c382e493186dd7c04
 OPENCCU_BASE_COMPAT_VERSION = 3.89.11
 OPENCCU_BASE_SITE = https://github.com/OpenCCU/OpenCCU-Base
 OPENCCU_BASE_SITE_METHOD = git
