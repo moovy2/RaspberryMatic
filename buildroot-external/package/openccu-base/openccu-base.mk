02292f6d30e17012815f5062586bae994f261d8e
Jens Maus <mail@jens-maus.de>
2026-09-22 22:11:41 +0200

    build(openccu-base): migrate patch 0177 (#4231)
    Completes the cleanup for OpenCCU-Base PR OpenCCU/OpenCCU-Base#52, which migrated rootfs patch 0177-WebUI-Fix-EmptyHmIPWKPPIN.patch into the native source.

--- buildroot-external/package/openccu-base/openccu-base.mk
+++ buildroot-external/package/openccu-base/openccu-base.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCCU_BASE_VERSION = 47852670b376000b91a5cfb7906dca84e2469aa4
+OPENCCU_BASE_VERSION = 2a34e0a82e603e725a40fbaefcb209c35b462fbc
 OPENCCU_BASE_COMPAT_VERSION = 3.89.11
 OPENCCU_BASE_SITE = https://github.com/OpenCCU/OpenCCU-Base
 OPENCCU_BASE_SITE_METHOD = git
