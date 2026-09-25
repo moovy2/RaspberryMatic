c9fe6f9bfaaf129ff613d4012e6a2243398f159f
Jens Maus <mail@jens-maus.de>
2026-09-25 22:35:18 +0200

    build(openccu-base): migrate patch 0157 (#4243)
    Completes the migration of rootfs patch 0157-WebUI-Fix-WrongJSONQuotes.patch after OpenCCU-Base PR #57 was merged.
    
    Signed-off-by: Jens Maus <mail@jens-maus.de>

--- buildroot-external/package/openccu-base/openccu-base.mk
+++ buildroot-external/package/openccu-base/openccu-base.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCCU_BASE_VERSION = d678401394f4e8c25abae5a0fa7c629f6647811a
+OPENCCU_BASE_VERSION = caec7573d289a4b36f322862f22b110903dddf45
 OPENCCU_BASE_COMPAT_VERSION = 3.89.11
 OPENCCU_BASE_SITE = https://github.com/OpenCCU/OpenCCU-Base
 OPENCCU_BASE_SITE_METHOD = git
